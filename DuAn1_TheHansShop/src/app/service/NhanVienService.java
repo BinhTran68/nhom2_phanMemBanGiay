dc07a1525cfd50e929cc429dbde7f11e968c5bab
BinhTran68 <binhtd2268@gmail.com>
2024-03-09 00:23:32 +0700

    cap nhat dang nhap va main du an

---
+++ DuAn1_TheHansShop/src/app/service/NhanVienService.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app.service;
+
+import app.model.NhanVien;
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class NhanVienService {
+
+    public NhanVien dangNhap(String email, String matKhau) {
+        return new NhanVien("Binh", "binh123@gmail.com", "binh123");
+    }
+    
+    
+}
