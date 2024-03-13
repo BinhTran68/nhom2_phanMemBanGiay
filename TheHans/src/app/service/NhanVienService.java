5037b372b0dea5d4fbd4d97645e440409334ed0f
BinhTran68 <binhtd2268@gmail.com>
2024-03-13 19:10:16 +0700

    cap nhat dang nhap

--- TheHans/src/app/service/NhanVienService.java
+++ TheHans/src/app/service/NhanVienService.java
@@ -7,6 +7,7 @@ package app.service;
 import app.model.NhanVien;
 import java.time.Instant;
 import java.util.Date;
+import java.sql.*;
 
 /**
  *
@@ -14,9 +15,72 @@ import java.util.Date;
  */
 public class NhanVienService {
 
-    public NhanVien dangNhap(String email, String matKhau) {
-        return new NhanVien("Binh", "binh123@gmail.com", "binh123");
+    PreparedStatement preparedStatement = null;
+
+    ResultSet resultSet = null;
+
+    String sql = "";
+
+    Connection connection = null;
+
+    public NhanVien dangNhap(String sdt, String matKhau) {
+
+        try {
+            connection = DBConnect.getConnection();
+            sql = "SELECT [id]\n"
+                    + "      ,[hoTen]\n"
+                    + "      ,[ngaySinh]\n"
+                    + "      ,[gioiTinh]\n"
+                    + "      ,[diaChi]\n"
+                    + "      ,[SDT]\n"
+                    + "      ,[email]\n"
+                    + "      ,[matKhau]\n"
+                    + "      ,[vaiTro]\n"
+                    + "      ,[trangThaiXoa]\n"
+                    + "      ,[ngayTao]\n"
+                    + "      ,[ngaySuaCuoi]\n"
+                    + "  FROM [dbo].[NhanVien] WHERE SDT = ? AND matKhau = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setObject(1, sdt);
+            preparedStatement.setObject(2, matKhau);
+            
+            resultSet = preparedStatement.executeQuery();
+            
+            if (resultSet == null) {
+                return  null;
+            }
+            NhanVien nhanVien = null;
+            while (resultSet.next()) {                
+                nhanVien = new NhanVien(
+                        resultSet.getInt(1), 
+                        resultSet.getString(2), 
+                        resultSet.getDate(3),
+                        resultSet.getInt(4),
+                        resultSet.getString(5),
+                        resultSet.getString(6),
+                        resultSet.getString(7),
+                        resultSet.getString(8),
+                        resultSet.getString(9), 
+                        resultSet.getBoolean(10),
+                        resultSet.getDate(11),
+                        resultSet.getDate(12));
+            
+            }
+               
+            return nhanVien;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                preparedStatement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
     }
-    
-    
+
 }
