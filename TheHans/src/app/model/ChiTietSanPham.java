0a53e1577214f1ab379b3197c85070afe4ed21e2
duycong192 <congldph46544@fpt.edu.vn>
2024-03-19 01:39:43 +0700

    cong sua ngon lanh cho hau

--- TheHans/src/app/model/ChiTietSanPham.java
+++ TheHans/src/app/model/ChiTietSanPham.java
@@ -1,31 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package app.model;
 
-
+/**
+ *
+ * @author admin
+ */
 public class ChiTietSanPham {
 
     private int id;
+    private String maCTSP;
     private String id_SanPham;
+    private double giaBan;
+    private int soLuongCon;
     private String id_MauSac;
     private String id_KichCo;
     private String id_Hang;
     private String id_ChatLieu;
     private String id_CTSP_KhuyenMai;
-    private String id_KhuyenMai;
-    private int soLuongCon;
-    private String maCTSP;
-    private String mota;
-    private double giaBan;
-    private String maVach;
-    private int trangThaiXoa;
     private String ngayTao;
     private String ngaySuaCuoi;
+    private int trangThaiXoa;
+    private String mota;
+    private String maVach;
 
-    public ChiTietSanPham(String string, String string0, double aDouble, int aInt, String string1, String string2, String string3, String string4, String string5, String string6, int aInt0, String string7, String string8) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public ChiTietSanPham() {
+    }
+
+    public ChiTietSanPham(String maCTSP, String id_SanPham, double giaBan, int soLuongCon, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, String ngayTao, String ngaySuaCuoi, int trangThaiXoa, String mota, String maVach) {
+        this.maCTSP = maCTSP;
+        this.id_SanPham = id_SanPham;
+        this.giaBan = giaBan;
+        this.soLuongCon = soLuongCon;
+        this.id_MauSac = id_MauSac;
+        this.id_KichCo = id_KichCo;
+        this.id_Hang = id_Hang;
+        this.id_ChatLieu = id_ChatLieu;
+        this.ngayTao = ngayTao;
+        this.ngaySuaCuoi = ngaySuaCuoi;
+        this.trangThaiXoa = trangThaiXoa;
+        this.mota = mota;
+        this.maVach = maVach;
+    }
+
+    public ChiTietSanPham(int id, String maCTSP, String id_SanPham, double giaBan, int soLuongCon, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, String id_CTSP_KhuyenMai, String ngayTao, String ngaySuaCuoi, int trangThaiXoa, String mota, String maVach) {
+        this.id = id;
+        this.maCTSP = maCTSP;
+        this.id_SanPham = id_SanPham;
+        this.giaBan = giaBan;
+        this.soLuongCon = soLuongCon;
+        this.id_MauSac = id_MauSac;
+        this.id_KichCo = id_KichCo;
+        this.id_Hang = id_Hang;
+        this.id_ChatLieu = id_ChatLieu;
+        this.id_CTSP_KhuyenMai = id_CTSP_KhuyenMai;
+        this.ngayTao = ngayTao;
+        this.ngaySuaCuoi = ngaySuaCuoi;
+        this.trangThaiXoa = trangThaiXoa;
+        this.mota = mota;
+        this.maVach = maVach;
+    }
+
+    public ChiTietSanPham(String maCTSP, String id_SanPham, double giaBan, int soLuongCon, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, String id_CTSP_KhuyenMai, int trangThaiXoa, String mota, String maVach) {
+        this.maCTSP = maCTSP;
+        this.id_SanPham = id_SanPham;
+        this.giaBan = giaBan;
+        this.soLuongCon = soLuongCon;
+        this.id_MauSac = id_MauSac;
+        this.id_KichCo = id_KichCo;
+        this.id_Hang = id_Hang;
+        this.id_ChatLieu = id_ChatLieu;
+        this.id_CTSP_KhuyenMai = id_CTSP_KhuyenMai;
+        this.trangThaiXoa = trangThaiXoa;
+        this.mota = mota;
+        this.maVach = maVach;
     }
 
     public ChiTietSanPham(String maCTSP, String id_SanPham, double giaBan, int soLuongCon, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, int trangThaiXoa, String mota, String maVach) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        this.maCTSP = maCTSP;
+        this.id_SanPham = id_SanPham;
+        this.giaBan = giaBan;
+        this.soLuongCon = soLuongCon;
+        this.id_MauSac = id_MauSac;
+        this.id_KichCo = id_KichCo;
+        this.id_Hang = id_Hang;
+        this.id_ChatLieu = id_ChatLieu;
+        this.trangThaiXoa = trangThaiXoa;
+        this.mota = mota;
+        this.maVach = maVach;
+    }
+public ChiTietSanPham(String id_SanPham, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, int soLuongCon, String maCTSP, double giaBan) {
+        this.id_SanPham = id_SanPham;
+        this.id_MauSac = id_MauSac;
+        this.id_KichCo = id_KichCo;
+        this.id_Hang = id_Hang;
+        this.id_ChatLieu = id_ChatLieu;
+        this.soLuongCon = soLuongCon;
+        this.maCTSP = maCTSP;
+        this.giaBan = giaBan;
+    }
+    @Override
+    public String toString() {
+        return "ChiTietSanPham{" + "id=" + id + ", maCTSP=" + maCTSP + ", id_SanPham=" + id_SanPham + ", giaBan=" + giaBan + ", soLuongCon=" + soLuongCon + ", id_MauSac=" + id_MauSac + ", id_KichCo=" + id_KichCo + ", id_Hang=" + id_Hang + ", id_ChatLieu=" + id_ChatLieu + ", id_CTSP_KhuyenMai=" + id_CTSP_KhuyenMai + ", ngayTao=" + ngayTao + ", ngaySuaCuoi=" + ngaySuaCuoi + ", trangThaiXoa=" + trangThaiXoa + ", mota=" + mota + ", maVach=" + maVach + '}';
     }
 
     public int getId() {
@@ -36,12 +114,36 @@ public class ChiTietSanPham {
         this.id = id;
     }
 
+    public String getMaCTSP() {
+        return maCTSP;
+    }
+
+    public void setMaCTSP(String maCTSP) {
+        this.maCTSP = maCTSP;
+    }
+
     public String getId_SanPham() {
         return id_SanPham;
     }
 
     public void setId_SanPham(String id_SanPham) {
         this.id_SanPham = id_SanPham;
+    }
+
+    public double getGiaBan() {
+        return giaBan;
+    }
+
+    public void setGiaBan(double giaBan) {
+        this.giaBan = giaBan;
+    }
+
+    public int getSoLuongCon() {
+        return soLuongCon;
+    }
+
+    public void setSoLuongCon(int soLuongCon) {
+        this.soLuongCon = soLuongCon;
     }
 
     public String getId_MauSac() {
@@ -84,62 +186,6 @@ public class ChiTietSanPham {
         this.id_CTSP_KhuyenMai = id_CTSP_KhuyenMai;
     }
 
-    public String getId_KhuyenMai() {
-        return id_KhuyenMai;
-    }
-
-    public void setId_KhuyenMai(String id_KhuyenMai) {
-        this.id_KhuyenMai = id_KhuyenMai;
-    }
-
-    public int getSoLuongCon() {
-        return soLuongCon;
-    }
-
-    public void setSoLuongCon(int soLuongCon) {
-        this.soLuongCon = soLuongCon;
-    }
-
-    public String getMaCTSP() {
-        return maCTSP;
-    }
-
-    public void setMaCTSP(String maCTSP) {
-        this.maCTSP = maCTSP;
-    }
-
-    public String getMota() {
-        return mota;
-    }
-
-    public void setMota(String mota) {
-        this.mota = mota;
-    }
-
-    public double getGiaBan() {
-        return giaBan;
-    }
-
-    public void setGiaBan(double giaBan) {
-        this.giaBan = giaBan;
-    }
-
-    public String getMaVach() {
-        return maVach;
-    }
-
-    public void setMaVach(String maVach) {
-        this.maVach = maVach;
-    }
-
-    public int getTrangThaiXoa() {
-        return trangThaiXoa;
-    }
-
-    public void setTrangThaiXoa(int trangThaiXoa) {
-        this.trangThaiXoa = trangThaiXoa;
-    }
-
     public String getNgayTao() {
         return ngayTao;
     }
@@ -156,40 +202,28 @@ public class ChiTietSanPham {
         this.ngaySuaCuoi = ngaySuaCuoi;
     }
 
-    public ChiTietSanPham() {
+    public int getTrangThaiXoa() {
+        return trangThaiXoa;
     }
 
-    public ChiTietSanPham(int id, String id_SanPham, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, String id_CTSP_KhuyenMai, String id_KhuyenMai, int soLuongCon, String maCTSP, String mota, double giaBan, String maVach, int trangThaiXoa, String ngayTao, String ngaySuaCuoi) {
-        this.id = id;
-        this.id_SanPham = id_SanPham;
-        this.id_MauSac = id_MauSac;
-        this.id_KichCo = id_KichCo;
-        this.id_Hang = id_Hang;
-        this.id_ChatLieu = id_ChatLieu;
-        this.id_CTSP_KhuyenMai = id_CTSP_KhuyenMai;
-        this.id_KhuyenMai = id_KhuyenMai;
-        this.soLuongCon = soLuongCon;
-        this.maCTSP = maCTSP;
-        this.mota = mota;
-        this.giaBan = giaBan;
-        this.maVach = maVach;
+    public void setTrangThaiXoa(int trangThaiXoa) {
         this.trangThaiXoa = trangThaiXoa;
-        this.ngayTao = ngayTao;
-        this.ngaySuaCuoi = ngaySuaCuoi;
     }
 
-    public ChiTietSanPham(String id_SanPham, String id_MauSac, String id_KichCo, String id_Hang, String id_ChatLieu, int soLuongCon, String maCTSP, double giaBan) {
-        this.id_SanPham = id_SanPham;
-        this.id_MauSac = id_MauSac;
-        this.id_KichCo = id_KichCo;
-        this.id_Hang = id_Hang;
-        this.id_ChatLieu = id_ChatLieu;
-        this.soLuongCon = soLuongCon;
-        this.maCTSP = maCTSP;
-        this.giaBan = giaBan;
+    public String getMota() {
+        return mota;
     }
-    
 
-    
+    public void setMota(String mota) {
+        this.mota = mota;
+    }
+
+    public String getMaVach() {
+        return maVach;
+    }
+
+    public void setMaVach(String maVach) {
+        this.maVach = maVach;
+    }
 
 }
